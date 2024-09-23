68a466c2f49e8bcac350ee4a2d019643e97b524b
Tony Okeke <muaddib@lisan-al-gaib>
2024-09-22 23:24:45 -0400

    feat: add css variables and animations

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: 'var(--accent-color)',
+        background: 'var(--background-color)',
+        border: 'var(--border-color)',
+        command: 'var(--text-command-color)',
+        highlight: 'var(--highlight-color)',
+        text: 'var(--text-color)',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': {opacity: '1'},
+          '50%': {opacity: '0'},
+        },
+      },
+      animation: {
+        blink: 'blink 1.0s step-end infinite',
+      },
+    },
   },
   plugins: [],
 };
