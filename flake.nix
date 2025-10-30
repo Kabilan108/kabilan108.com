901f003a1ad9e3bfa9f0fc55c22dc3808f8ddceb
Tony Okeke <tonykabilanokeke@gmail.com>
2025-10-29 22:53:30 -0400

    build: update flake - pnpm, node 22 & chromium

--- flake.nix
+++ flake.nix
@@ -1,18 +1,15 @@
 {
-  description = "";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-
+  description = "node 22 + pnpm dev shell w chromium";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
     pkgs   = import nixpkgs { inherit system; };
   in {
     devShell.${system} = pkgs.mkShell {
-      buildInputs = [
-        pkgs.nodejs_22
-        pkgs.bun
+      buildInputs = with pkgs; [
+        nodejs_22
+        pnpm
+        chromium
       ];
 
       shellHook = ''
