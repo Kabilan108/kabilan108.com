5554b4d1505d4950da849b6a79c6d17cb22d0ff0
Tony Okeke <tonykabilanokeke@gmail.com>
2026-03-21 18:56:44 -0400

    build: set up prek

--- flake.nix
+++ flake.nix
@@ -1,20 +1,37 @@
 {
-  description = "node 22 + pnpm dev shell w chromium";
+  description = "bun dev shell w chromium";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs   = import nixpkgs { inherit system; };
-  in {
-    devShell.${system} = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        nodejs_22
-        pnpm
-        chromium
-      ];
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      prek-custom = pkgs.stdenv.mkDerivation {
+        pname = "prek";
+        version = "0.3.5";
+        src = pkgs.fetchurl {
+          url = "https://github.com/j178/prek/releases/download/v0.3.5/prek-x86_64-unknown-linux-gnu.tar.gz";
+          sha256 = "sha256-vuHD9VCvAEqk2kRBZIGU6GLaYpoCNh2yYPz1gx1Km9k=";
+        };
+        sourceRoot = ".";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp prek-x86_64-unknown-linux-gnu/prek $out/bin/
+          chmod +x $out/bin/prek
+        '';
+      };
+    in
+    {
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          bun
+          chromium
+          prek-custom
+        ];
 
-      shellHook = ''
-        export CHROMIUM_BINARY="${pkgs.chromium}/bin/chromium"
-      '';
+        shellHook = ''
+          export CHROMIUM_BINARY="${pkgs.chromium}/bin/chromium"
+        '';
+      };
     };
-  };
 }
